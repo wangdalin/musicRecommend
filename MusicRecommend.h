f61cf252af6fd1618b6d821a6f443b55409295dc
wangdalin <39913209+wangdalin@users.noreply.github.com>
2018-06-05 01:26:36 +0800

    Update MusicRecommend.h

--- MusicRecommend.h
+++ MusicRecommend.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef LIBRARY
 #define LIBRARY value
 
@@ -6,159 +7,158 @@
 #include <vector>
 using namespace std;
 /*操作数*/
+enum RANK
+{listener,admin,vip
 
+};
 typedef bool Status;
 
 
-class MusicDatabase{
+class MusicDatabase {
+public:
 
-	public:
+	MusicDatabase();
+	MusicDatabase(string);
+	MusicDatabase(const MusicDatabase &m);
+	~MusicDatabase(){}
 
-		MusicDatabase();
-		MusicDatabase(const MusicDatabase &m);
-		~MusicDatabase();
+	Status InsertUser(string user_id, string name, RANK rank, string password);
+	Status DeleteUser(string user_id);
+	Status UpdatePassword(string uer_name, string new_password);
+	Status InsertMusic(string musicName, string Singer, string id, string lyrics);
+	Status DeleteMusic(string music_id);
+	Status InsertComment(string music_id, string user_id, string comment, int num);
+	Status DeleteComment(string music_id, string user_id, int num);
+	Status GetUser(string user_id);
+	Status GetMusic(string music_id);
 
-		Status InsertUser(string user_id, string name, string rank, string password);
-		Status DeleteUser(string user_id);
-		Status UpdatePassword(string uer_name, string new_password);
-		Status InsertMusic(string musicName,string Singer,string id, string lyrics);		
-		Status DeleteMusic(string music_id);
-		Status InsertComment(string music_id, string user_id, string comment, int num);
-		Status DeleteComment(string music_id, string user_id, int num);
-		Status GetUser(string user_id);
-		Status GetMusic(string music_id);
+	int countComment(string user_id);
 
-		int countComment(string user_id);
+	string MusicName2Id(string music_name);
+	string UserName2Id(string user_name);
+	leveldb::DB* Getdb() {
+		return db;
+	}
+private:
+	leveldb::DB* db_passwd;
+	leveldb::DB* db_username;
+};
 
-		string MusicName2Id(string music_name);				
-		string UserName2Id(string user_name);
+class Recommand {
+
+public:
+
+	Recommand();
+	~Recommand();
+
+	vector<int> Word2Vec(string word);
+	string GetMusic(vector<int> vec);
 
 };
 
-class Recommand{
-
-	public:
-		
-		Recommand();
-		~Recommand();
-		
-		vector<int> Word2Vec(string word);
-		string GetMusic(vector<int> vec);
-
-};
-
-//class Judge{
-
-	//public:
-		
-		//Judge();
-		//~Judge();
-		
-//		double precision_at_k(ranklists, k);
-	//	double mean_average_precision(ranklists, k);
-		//double ndcg_at_k(rank_list, k);
-	//	double ideal_dcg(rank_list, k);
-
-//};
 
 
 class User
 {
-	public:
-		
-		User();
-		~User();
-		
-		virtual Status Comment(string music_name, string comment);
-		virtual Status DownloadLyric(string music_name);
-		virtual Status passwd(string new_password);
-		Status Search(string music);      //    Search from all the tables
+public:
+
+	User() {}
+	~User() {}
+
+	/*virtual Status Comment(string music_name, string comment);
+	virtual Status DownloadLyric(string music_name);
+	virtual Status Passwd(string new_password);*/
+	Status Search(string music);      //    Search from all the tables
+	Status SetUserName(string);
+	Status SetPasswd(string);
+	Status SetUserId(string);
+	string GetUserName();
+	string GetPasswd();
+	string GetUserId();
+
+protected:
+	string user_name_;
+	string passwd_;
+	string user_id_;
+	RANK rank_;
+};
 
 
+class Admin : public User {
 
-	private:
+public:
 
-		string user_;
-		string passwd_;
+	Admin();
+	~Admin();
+	Status Comment(string music_name, string comment);
+	Status DownloadLyric(string music_name);
+	Status passwd(string new_password);
+	Status DeleteComment(string music_name, string comment);
+	Status DeleteMusic(string music_name);
+
+
+};
+
+class Listener :public User {
+
+public:
+
+	Listener() {}
+	~Listener() {}
+
+	Status Comment(string music_name, string comment);
+	Status DownloadLyric(string music_name);
+	Status passwd(string new_password);
 
 };
 
 
-class Admin: public User{
+class Vip :public User {
 
-	public:
-	
-		Admin();
-		~Admin();
-		Status Comment(string music_name, string comment);
-		Status DownloadLyric(string music_name);
-		Status passwd(string new_password);
-		Status DeleteComment(string music_name, string comment);
-		Status DeleteMusic(string music_name);
+public:
 
-
-};
-
-class Listener:public User{
-
-	public:
-	
-		Listener();
-		~Listener();
-
-		Status Comment(string music_name, string comment);
-		Status DownloadLyric(string music_name);
-		Status passwd(string new_password);
-
-};
-
-
-class Vip:public User{
-
-	public:
-
-		Vip();
-		~Vip();
-		Status Comment(string music_name, string comment);
-		Status DownloadLyric(string music_name);
-		Status passwd(string new_password);
-		Status RewriteComment(music_id, string new_comment, int num);
+	Vip();
+	~Vip();
+	Status Comment(string music_name, string comment);
+	Status DownloadLyric(string music_name);
+	Status passwd(string new_password);
+	Status RewriteComment(string music_id, string new_comment, int num);
 
 };
 
 
 
-class Music{
+class Music {
 
-	public:
+public:
 
-		Music();
-		~Music();
+	Music();
+	~Music();
 
-	private:
+private:
 
-		string name_;
-		string singer_;
-		int id_;
-		string lyric_;
+	string name_;
+	string singer_;
+	int id_;
+	string lyric_;
 
 };
 
 
-class Comment{
+class Comment {
 
-	public:
-	
-		Comment();
-		Comment(string commnet_, username_, music_, int num_);
-		~Comment();
-		
-	private:
+public:
 
-		string comment_;
-		string username_;
-		string music_;
-		int num_;
+	Comment();
+	Comment(string commnet_, string username_, string music_, int num_);
+	~Comment();
+
+private:
+
+	string comment_;
+	string username_;
+	string music_;
+	int num_;
 
 };
 
